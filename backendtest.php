818875a692242d54e80d584d0edb4820b383859e
Henry Wilson <henryhwilson@gmail.com>
2015-04-11 21:50:10 -0400

    print_r

--- backendtest.php
+++ backendtest.php
@@ -3,6 +3,7 @@ $user= 'robert.y.sayegh.18@dartmouth.edu';
 $pass='Dummy123';
 $response = exec(dirname(__FILE__) . "phantomjs ". dirname(__FILE__) . "login.js $user $pass", $output, $return);
 echo print_r($output);
+echo dirname(__FILE__);
 echo $output[0];
 if (!$return) {
 	echo "Created successfully";

3d98d6a34be36a20f91a60e8755ef93d727495e5
Henry Wilson <henryhwilson@gmail.com>
2015-04-11 21:47:55 -0400

    print_r

--- backendtest.php
+++ backendtest.php
@@ -1,7 +1,8 @@
 <?php
 $user= 'robert.y.sayegh.18@dartmouth.edu';
 $pass='Dummy123';
-$response = exec("phantomjs login.js $user $pass", $output);
+$response = exec("phantomjs login.js $user $pass", $output, $return);
 echo print_r($output);
 echo $output[0];
+echo $return;
 ?>

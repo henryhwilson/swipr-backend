fd0c5fc53e1c90bb6ab3265fc650b8582571e730
Henry Wilson <henryhwilson@gmail.com>
2015-04-11 21:46:32 -0400

    print_r

--- backendtest.php
+++ backendtest.php
@@ -1,7 +1,7 @@
 <?php
 $user= 'robert.y.sayegh.18@dartmouth.edu';
 $pass='Dummy123';
-$response = exec("/home/jef28/public_html/swipr/phantomjs /home/jef28/public_html/swipr/login.js $user $pass", $output);
+$response = exec("phantomjs login.js $user $pass", $output);
 echo print_r($output);
 echo $output[0];
 ?>
